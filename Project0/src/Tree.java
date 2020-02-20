b084347a80fa19066acf8dd8787018bd37dac52b
unknown <christopher.a.cruzen@gmail.com>
2020-02-20 01:05:37 -0600

    Remove Unused Imports

--- Project0/src/Tree.java
+++ Project0/src/Tree.java
@@ -8,11 +8,8 @@
  * construction and traversal logic with utility output methods.
  */
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class Tree {
 
