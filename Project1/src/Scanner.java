a825cc0f5bfdf60ea2d7d7abcd5309ab6bb4e52d
unknown <christopher.a.cruzen@gmail.com>
2020-04-05 17:48:15 -0500

    Implement Basic Scanner Algorithm

--- Project1/src/Scanner.java
+++ Project1/src/Scanner.java
@@ -9,9 +9,6 @@
  */
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Scanner {
 
 
@@ -19,6 +16,7 @@ public class Scanner {
 
     private FsaTable fsaTable;
     private Filter filter;
+    private boolean scanComplete = false;
 
 
     /*--- Constructor ---*/
@@ -26,6 +24,8 @@ public class Scanner {
     public Scanner(java.util.Scanner inputScanner) {
         this.fsaTable = new FsaTable();
         this.filter = new Filter(inputScanner);
+
+
     }
 
 
@@ -33,19 +33,62 @@ public class Scanner {
 
     // Returns the next available token, or null if no further tokens are available.
     public Token getNextToken() {
-        FilteredCharacter currentCharacter = filter.getCurrentCharacter();
-        FilteredCharacter lookAheadCharacter = filter.getLookAheadCharacter();
-        filter.iterate();
 
-
-        if (currentCharacter == null) {
+        // Short Circuit when Complete
+        if (scanComplete) {
             return null;
-        } else {
-            return new Token(
-                    Token.Type.Undefined,
-                    currentCharacter.getCharacter() + "",
-                    currentCharacter.getLine()
-            );
+        }
+
+        // Create Variables
+        String tokenInstance = "";
+        FilteredCharacter currentCharacter;
+        FilteredCharacter lookAheadCharacter;
+        int state = 0;
+        int nextState = 0;
+
+        // Begin Assembly Loop
+        while (true) {
+
+            // Get Characters
+            currentCharacter = filter.getCurrentCharacter();
+            lookAheadCharacter = filter.getLookAheadCharacter();
+
+            // Handle Final Character
+            if (lookAheadCharacter == null) {
+                if (tokenInstance.isEmpty()) {
+                    scanComplete = true;
+                    return new Token(Token.Type.EndOfFile, "", currentCharacter.getLine());
+                } else {
+                    return new Token(Token.Type.Undefined, tokenInstance + currentCharacter.getCharacter(), currentCharacter.getLine());
+                }
+            }
+
+            // Get State
+            state = fsaTable.getNextState(state, currentCharacter.getCharacter());
+            nextState = fsaTable.getNextState(state, lookAheadCharacter.getCharacter());
+
+            // Check Initial State For Error
+            if (state == -1) {
+                System.out.println("SCANNER ERROR: Invalid character '" + currentCharacter.getCharacter() + "' at line " + currentCharacter.getLine());
+                System.exit(1);
+            }
+
+            // Check Next State For Error
+            if (nextState == -1) {
+                System.out.println("SCANNER ERROR: Invalid character '" + lookAheadCharacter.getCharacter() + "' at line " + lookAheadCharacter.getLine());
+                System.exit(1);
+            }
+
+            // Update Token Instance
+            tokenInstance += currentCharacter.getCharacter();
+
+            filter.iterate();
+
+            // Check for Token End
+            if (nextState > 500) {
+                // Token Complete. Return.
+                return new Token(Token.Type.Undefined, tokenInstance, currentCharacter.getLine());
+            }
         }
     }
 }
